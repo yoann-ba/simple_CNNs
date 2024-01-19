a96ac29d46dbff1fe2659209af7110683a8d9bdd
Yoann B <126958043+yoann-ba@users.noreply.github.com>
2024-01-19 15:58:32 +0100

    Create cnn_sandbox.py

---
+++ cnn_sandbox.py
@@ -0,0 +1,146 @@
+
+
+
+
+
+#%% imports
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from keras import layers
+from keras import ops
+
+
+
+#%% densenet
+
+
+
+
+
+
+
+
+
+
+
+#%% convnext
+
+# 
+def ConvNextBlock():
+    
+    aa
+    
+    return
+
+
+# 
+def ConvNext():
+    
+    aa
+    
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
