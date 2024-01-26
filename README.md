e7c741e42d9f0ebb31ad07efcd8e24883a4c9db2
Yoann B <126958043+yoann-ba@users.noreply.github.com>
2024-01-26 13:48:09 +0100

    upd convnext

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # simple_CNNs
+
+Simple CNN blocks for easier re-use of small parts for when importing a full model isn't viable (anywhere people throw in a VGG or ResNet blindly). Originally I was going to do ResNet block -> DenseNet block -> ConvNext block to scale up design complexity and performance but the design of the ConvNext is basically as simple as the ResNet's if not even simpler, so I'll just focus on it.
+
+ConvNext v1 and not v2.
+
+[some links I guess]
--- cnn_sandbox.py
+++ cnn_sandbox.py
@@ -13,32 +13,36 @@ from keras import ops
 
 
 
-#%% densenet
-
-
-
-
-
-
-
-
-
-
-
 #%% convnext
 
 # 
 def ConvNextBlock():
     
-    aa
+    # depthwise conv, 7x7, 96 channels
+    # Layer Norm
+    # 1x1 conv / Dense layer, 384 channels (x4)
+    # GELU (/ReLU)
+    # 1x1 conv / Dense layer, 96 channels
+    # add inputs
     
     return
 
 
 # 
+# ConvNext is always 4 stages of img resolution
+# nb of blocks per stages is either (3, 3, 9, 3) or (3, 3, 27, 3)
+# nb of channels in the blocks goes x2 every stage
+# beginning is a patchify stem?
 def ConvNext():
     
-    aa
+    # stem
+    
+    # other preprocess?
+    
+    # for each stage
+    # for each block
+    
+    # output/postprocess
     
     return
 
