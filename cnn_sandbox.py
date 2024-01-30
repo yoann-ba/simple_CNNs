21cb6aaa62716060784e9e2833e71d04cfb312c6
Yoann B <126958043+yoann-ba@users.noreply.github.com>
2024-01-30 12:15:58 +0100

    Update cnn_sandbox.py

--- cnn_sandbox.py
+++ cnn_sandbox.py
@@ -42,10 +42,10 @@ def ConvNextBlock(x, nb_c, model_name):
     # Layer Scale?
     # Stochastic Depth
     
-    
     return x
 
 # 
+# reduces intial input data by 4
 def Stem(x, nb_c, model_name):
     
     x = layers.Conv2D(nb_c, 
@@ -59,14 +59,35 @@ def Stem(x, nb_c, model_name):
 
 # 
 #TODO add stage nb to name
-def Downsampling(x, nb_c, model_name):
+# if we don't want to downsample then it comes out same size but half reduced in details
+def Downsampling(x, nb_c, model_name, downsample = True):
     
     x = layers.LayerNormalization(epsilon = 1e-6, 
                                   name = model_name + '_downsampling_layernorm')(x)
-    x = layers.Conv2D(filters = nb_c, 
+    if downsample:
+        x = layers.Conv2D(filters = nb_c, 
                       kernel_size = 2, 
                       strides = 2, 
                       name=model_name + '_downsampling_conv')(x)
+    else:
+        x = layers.MaxPooling2D(padding='same', strides = (1, 1), 
+                                name = model_name + '_same_size_maxpool')(x)
+        # x = layers.Conv2D(filters = nb_c, 
+        #                   kernel_size = 2, 
+        #                   name = model_name + '_same_size_conv')(x)
+    # if
+    
+    return x
+
+# 
+#TODO parametrize classif activation function?
+def ClassifierHead(x, nb_classes, model_name):
+    
+    x = layers.GlobalAveragePooling2D(name=model_name + "_head_gap")(x)
+    x = layers.LayerNormalization(epsilon=1e-6, 
+                                  name=model_name + "_head_layernorm")(x)
+    x = layers.Dense(nb_classes, activation="softmax", 
+                     name=model_name + "_head_dense")(x)
     
     return x
 
@@ -77,14 +98,13 @@ def Downsampling(x, nb_c, model_name):
 # beginning is a patchify stem? 4x4 non overlap convolution
 
 # just deduce the number of stages from the length of nb blocks
-#TODO add parameter to decide whether we downscale or not
-#TODO problably parametrize the Head
 #TODO stochastic depth
 #TODO layer scale
 #TODO float16?
 def ConvNext(nb_blocks = (3, 3, 9, 3), 
              nb_channels = (96, 192, 384, 768), 
-             input_shape = None, 
+             input_shape = None, classif_head = False, nb_classes = None, 
+             downsample = True, 
              model_name = 'homemade_convnext'):
     
     inputs = layers.Input(shape=input_shape)
@@ -99,7 +119,6 @@ def ConvNext(nb_blocks = (3, 3, 9, 3),
     
     # for each stage
     for i_s in range(len(nb_blocks)):
-        
         # for each block
         for i_b in range(nb_blocks[i_s]):
             x = ConvNextBlock(x, nb_c=nb_channels[i_s], model_name=model_name)
@@ -108,14 +127,21 @@ def ConvNext(nb_blocks = (3, 3, 9, 3),
     # end stage loop
     
     # output/postprocess
+    if classif_head == True:
+        if nb_classes != None:
+            x = ClassifierHead(x, nb_classes, model_name=model_name)
+    # 
     
     model = Functional(inputs = inputs, outputs = x, name = model_name)
     
     return model
 
-
-
-
+# Training:
+# AdamW opt
+# 300 epochs
+# data aug: MixUp, CutMix, RandAugment, Random Erasing
+# regularization: Stochastic Depth, Label Smoothing
+# full settings of hyperparameters for pre-training and fine-tuning available in Appendix
 
 
 
