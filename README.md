05da2bfb16ff8f0ede319d3aadeebe1fd4b8323f
Yoann B <126958043+yoann-ba@users.noreply.github.com>
2024-02-01 16:16:40 +0100

    layer scale and stochastic depth

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ ConvNext v1 and not v2 (for now?).
 I realize I could have just imported the keras.applications.convnext.py file and re-built juste the global wrapper/model building function, but at least now everything is clear and it's more independant from the rest of the system. Still needs to import keras to work but as long as the system/environment is capable of reproducing the 4-5 operations in there you could re-create it easily.
 
 Differences: 
-- No Layer Scale that's in the paper and the keras official implementation
-- No Stochastic Depth that's in the paper and the keras official implementation
 - No float16/other dtype optimisation (no info about it on the official?)
 - No data/inputs check or verification that are in the official implementation
 - No pre-trained weights obviously
+- Added an option to not downsample after the stem (keeps at 1/4 the whole time)
+- Can choose the number of stages properly
 - 
 
 [Notebook where I'm testing it](https://colab.research.google.com/drive/1mFFt1J2YfxRiJlon7fYS5cku7xRpFYCl?usp=sharing)
--- simple_convnext.py
+++ simple_convnext.py
@@ -5,8 +5,100 @@
 
 #%% imports
 
+import numpy as np
+
 import keras
 from keras import layers
+
+# for custom layers
+from keras.layers.layer import Layer
+from keras import ops, random # for stochastic depth
+from keras import initializers # for layer scale
+
+
+#%% Custom layers
+# taken blindly from 
+# https://github.com/keras-team/keras/blob/v3.0.2/keras/applications/convnext.py
+
+
+class StochasticDepth(Layer):
+    """Stochastic Depth module.
+
+    It performs batch-wise dropping rather than sample-wise. In libraries like
+    `timm`, it's similar to `DropPath` layers that drops residual paths
+    sample-wise.
+
+    References:
+    - https://github.com/rwightman/pytorch-image-models
+
+    Args:
+      drop_path_rate (float): Probability of dropping paths. Should be within
+        [0, 1].
+
+    Returns:
+      Tensor either with the residual path dropped or kept.
+    """
+
+    def __init__(self, drop_path_rate, **kwargs):
+        super().__init__(**kwargs)
+        self.drop_path_rate = drop_path_rate
+
+    def call(self, x, training=None):
+        if training:
+            keep_prob = 1 - self.drop_path_rate
+            shape = (ops.shape(x)[0],) + (1,) * (len(ops.shape(x)) - 1)
+            random_tensor = keep_prob + random.uniform(shape, 0, 1)
+            random_tensor = ops.floor(random_tensor)
+            return (x / keep_prob) * random_tensor
+        return x
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"drop_path_rate": self.drop_path_rate})
+        return config
+
+
+# essentially x <- x * gamma (~1e-6)
+class LayerScale(Layer):
+    """Layer scale module.
+
+    References:
+
+    - https://arxiv.org/abs/2103.17239
+
+    Args:
+        init_values (float): Initial value for layer scale. Should be within
+            [0, 1].
+        projection_dim (int): Projection dimensionality.
+
+    Returns:
+        Tensor multiplied to the scale.
+    """
+
+    def __init__(self, init_values, projection_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.init_values = init_values
+        self.projection_dim = projection_dim
+
+    def build(self, _):
+        self.gamma = self.add_weight(
+            shape=(self.projection_dim,),
+            initializer=initializers.Constant(self.init_values),
+            trainable=True,
+        )
+
+    def call(self, x):
+        return x * self.gamma
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "init_values": self.init_values,
+                "projection_dim": self.projection_dim,
+            }
+        )
+        return config
 
 
 
@@ -14,7 +106,8 @@ from keras import layers
 
 # 
 # Small -> Large -> Small pattern
-def ConvNextBlock(x, nb_c, model_name):
+def ConvNextBlock(x, nb_c, model_name, 
+                  layer_scale_init_value = 1e-6, drop_path_rate = 0.0):
     
     # depthwise grouped conv, 7x7, 96 channels
     x2 = layers.Conv2D(filters = nb_c, 
@@ -31,11 +124,22 @@ def ConvNextBlock(x, nb_c, model_name):
     # 1x1 conv / Dense layer, 96 channels
     x2 = layers.Dense(nb_c, name = model_name + "_pointwise_conv_2")(x2)
     
+    # Layer Scale, i.e x = x * gamma
+    if layer_scale_init_value is not None:
+        x = LayerScale(layer_scale_init_value,
+                       nb_c, name = model_name + "_layer_scale")(x)
+    # Stochastic Depth
+    if drop_path_rate:
+        x = StochasticDepth(drop_path_rate, 
+                            name = model_name + "_stochastic_depth")(x)
+    else:
+        x = layers.Activation("linear", 
+                              name = model_name + "_identity")(x)
+    # --
+    
     # bind the skip connection
     x = x + x2
     
-    # Layer Scale?
-    # Stochastic Depth
     
     return x
 
@@ -66,9 +170,6 @@ def Downsampling(x, nb_c, model_name, downsample = True):
     else:
         x = layers.MaxPooling2D(padding='same', strides = (1, 1), 
                                 name = model_name + '_same_size_maxpool')(x)
-        # x = layers.Conv2D(filters = nb_c, 
-        #                   kernel_size = 2, 
-        #                   name = model_name + '_same_size_conv')(x)
     # if
     
     return x
@@ -92,34 +193,38 @@ def ClassifierHead(x, nb_classes, model_name):
 # beginning is a patchify stem? 4x4 non overlap convolution
 
 # just deduce the number of stages from the length of nb blocks
-#TODO stochastic depth
-#TODO layer scale
 #TODO float16?
 def ConvNext(nb_blocks = (3, 3, 9, 3), 
              nb_channels = (96, 192, 384, 768), 
              input_shape = None, classif_head = False, nb_classes = None, 
              downsample = True, 
+             layer_scale_init_value = 1e-6, drop_path_rate = 0.0, 
              model_name = 'homemade_convnext'):
     
     inputs = layers.Input(shape=input_shape)
     x = inputs
     
-    # pre stem (standard scaling according to some mu and sigma)
-    # just do it on your data before feeding it here
-    # or declare another model that goes preproc->this model
-    
     # stem
     x = Stem(x, nb_c=nb_channels[0], model_name=model_name)
     
+    # Block 0 get 0% of the drop rate, 
+    # Block n gets 100% of input drop rate, linear in between
+    depth_drop_rates = [float(x) for x in np.linspace(0.0, drop_path_rate, sum(nb_blocks))]
+    
+    block_counter = 0
     # for each stage
     for i_s in range(len(nb_blocks)):
         # for each block
         for i_b in range(nb_blocks[i_s]):
             x = ConvNextBlock(x, nb_c=nb_channels[i_s], 
+                              layer_scale_init_value=layer_scale_init_value, 
+                              drop_path_rate=depth_drop_rates[block_counter], 
                               model_name=f"{model_name}_s{i_s+1}_b{i_b+1}")
+            block_counter += 1
         if i_s < len(nb_blocks)-1: # no downsample on the last one
             x = Downsampling(x, nb_c=nb_channels[i_s + 1], 
                              model_name=f"{model_name}_s{i_s+1}_b{i_b+1}")
+            # 
     # end stage loop
     
     # output/postprocess
